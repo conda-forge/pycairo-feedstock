f4086d04b021d9ed8cd64cd891f41e960fdd662a
Ryan Volz <rvolz@mit.edu>
2020-03-18 12:56:24 -0400

    Build with meson on Windows so that pkg-config files are provided.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,33 @@
-set "INCLUDE=%LIBRARY_PREFIX%\include\cairo;%INCLUDE%"
-%PYTHON% -m pip install --no-deps --ignore-installed -v .
+setlocal EnableDelayedExpansion
+@echo on
+
+:: meson options
+:: (set pkg_config_path so deps in host env can be found)
+set ^"MESON_OPTIONS=^
+  --prefix="%LIBRARY_PREFIX%" ^
+  --pkg-config-path="%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig" ^
+  --wrap-mode=nofallback ^
+  --buildtype=release ^
+  --backend=ninja ^
+  -D python=%PYTHON% ^
+ ^"
+
+:: configure build using meson
+%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson setup builddir !MESON_OPTIONS!
 if errorlevel 1 exit 1
+
+:: print results of build configuration
+%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson configure builddir
+if errorlevel 1 exit 1
+
+ninja -v -C builddir
+if errorlevel 1 exit 1
+
+ninja -C builddir install
+if errorlevel 1 exit 1
+
+:: meson doesn't put the Python files in the right place, and there's no way to override
+cd %LIBRARY_PREFIX%\lib\python*
+cd site-packages
+move *.egg-info %PREFIX%\Lib\site-packages
+move cairo %PREFIX%\Lib\site-packages\cairo
