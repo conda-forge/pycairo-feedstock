85c0756c612659dee6d5481e6e6561f30ee95f8d
Ryan Volz <rvolz@mit.edu>
2020-03-19 18:22:15 -0400

    Use meson for Unix build as well so that .pc files are provided.
    Because the pkg-config files for pycairo were not previously included in
    this package, the build for pygobject has been failing to locate pycairo
    and building it instead as an in-tree subproject. This has resulted in
    some pycairo files being included in the pygobject package.

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,4 +8,25 @@ if [ $(uname) = Darwin ] ; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
-python setup.py install
+# We're using the meson build system as opposed to 'pip install .' because it provides
+# the pkg-config files for `pycairo`. These are used by downstream packages (notably
+# `pygobject`) that also use the meson build system in order to locate `pycairo`.
+# Without them, `pycairo` will not be found and might be built as an in-tree subproject
+# which is obviously undesirable.
+
+# meson options
+meson_config_args=(
+  --prefix="$PREFIX"
+  --libdir=lib
+  --wrap-mode=nofallback
+  --buildtype=release
+  --backend=ninja
+  -D python="$PYTHON"
+)
+
+# configure build using meson
+meson setup builddir "${meson_config_args[@]}"
+
+ninja -v -C builddir -j ${CPU_COUNT}
+ninja -C builddir test
+ninja -C builddir install
